c45e434fb006ab34afc7ca7bf169a644ff5b16f1
mohaned <mmagdy@sigmaproit.com>
2018-03-19 14:56:49 +0200

    Interpolation, Property & Event Binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  itemsCount: number = 4;
+  btnText: string = "Add an item";
+  goalText: string = "My life goal"; 
+  goals = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemsCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemsCount = this.goals.length;
   }
 
 }
